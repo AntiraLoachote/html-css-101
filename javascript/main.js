251e315aadbc99ac8a37dd381074ace87a0f35d7
Anpan <antirapan26@gmail.com>
2021-07-18 17:37:24 +0700

    Add Javascript!

---
+++ javascript/main.js
@@ -0,0 +1,61 @@
+// alert("Hello world");
+
+// What you will learn in this course
+// 0. Intro programming logic
+
+// 1. Variables & Data Types
+
+// Variables
+// var age = 30;
+// console.log(age);
+
+// let name = "Anpan";
+// name = "Olaf";
+// console.log(name);
+
+// const value = 10;
+// // value = 10;
+// console.log(value);
+
+// // Data Types
+// // String, Number, Boolean, null, undefined
+let meesage = "Welcome";
+const weight = 30;
+const rating = 4.5;
+const isShow = true;
+const x = null;
+const y = undefined;
+let z;
+// const z = "test";
+
+console.log(typeof meesage, meesage);
+// console.log(typeof weight, weight);
+// console.log(typeof rating, rating);
+// console.log(typeof isShow, isShow);
+// console.log(typeof x, x);
+// console.log(typeof y, y);
+// console.log(typeof z, z);
+
+meesage = "wow";
+console.log(typeof meesage, meesage);
+
+// // Concatenation of string
+// const text = "My name is" + name + ".";
+// console.log("text", text);
+
+// // Template String
+// const helloMessage = `My name is ${name}.`;
+// console.log("helloMessage", helloMessage);
+
+// // 2. Arrays
+// // 3. Object Literals
+// // 4. Methods for strings, arrays, objects, etc
+// // 5. Loops - for, while, for..of, forEach, map
+// // 6. Conditionals (if, ternary & switch)
+// // 7. Functions (normal & arrow)
+
+// // 8. OOP (prototypes & classess)
+// // 9. DOM Selection
+// // 10. DOM manipulation
+// // 11. Events
+// // 12. Basic Form Validation
---
+++ javascript/outline.js
@@ -0,0 +1,25 @@
+// What is Javascript?
+// - High level, interpreted programming language
+// - Conforms to tje ECMAScript specification
+// - Multi-paradigm
+// - Runs on the client/browser as well as on the server (Node.js)
+
+// Why learn Javascript?
+// - It is programming language of the browser
+// - Build very interactive user interfaces with frameworks like React
+// - Used in building very fast server side and full stack applications
+// - Used in mobile development and desktop app
+
+// What you will learn in this course
+// 1. Variables & Data Types
+// 2. Arrays
+// 3. Object Literals
+// 4. Methods for strings, arrays, objects, etc
+// 5. Loops - for, while, for..of, forEach, map
+// 6. Conditionals (if, ternary & switch)
+// 7. Functions (normal & arrow)
+// 8. OOP (prototypes & classess)
+// 9. DOM Selection
+// 10. DOM manipulation
+// 11. Events
+// 12. Basic Form Validation
